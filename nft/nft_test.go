bcdd62c29562a622a78d03ed2717155fd7fe0009
Richard Lloyd <relloyd@gmail.com>
2024-11-25 20:55:33 +0000

    uncomment all tests OK

--- nft/nft_test.go
+++ nft/nft_test.go
@@ -28,20 +28,20 @@ func Test_SendIP4PacketsToDefaultNFQueue(t *testing.T) {
 	err = nfq.conn.Flush()
 	assert.NoError(t, err, "Flush() error = %v", err)
 
-	// // Check length of chain rules.
-	// r, err := nfq.conn.GetRules(nfq.table, nfq.chain)
-	// t.Log("num rules = ", r)
-	// assert.NoError(t, err, "conn.GetRules() error = %v", err)
-	// assert.Equal(t, 1, len(r), "expected 1 rule")
-	//
-	// // Add empty rules list which should cause chain flush.
-	// err = nfq.SendIP4PacketsToDefaultNFQueue([]string{""}) // send empty IP list to cause empty rules.
-	// assert.NoError(t, err, "SendIP4PacketsToDefaultNFQueue() error = %v", err)
-	//
-	// // Check length of chain rules.
-	// r, err = nfq.conn.GetRules(nfq.table, nfq.chain)
-	// assert.NoError(t, err, "conn.GetRules() error = %v", err)
-	// assert.Equal(t, 0, len(r), "SendIP4PacketsToDefaultNFQueue() chain should be empty")
+	// Check length of chain rules.
+	r, err := nfq.conn.GetRules(nfq.table, nfq.chain)
+	t.Log("num rules = ", r)
+	assert.NoError(t, err, "conn.GetRules() error = %v", err)
+	assert.Equal(t, 1, len(r), "expected 1 rule")
+
+	// Add empty rules list which should cause chain flush.
+	err = nfq.SendIP4PacketsToDefaultNFQueue([]string{}) // send empty IP list to cause empty rules.
+	assert.NoError(t, err, "SendIP4PacketsToDefaultNFQueue() error = %v", err)
+
+	// Check length of chain rules.
+	r, err = nfq.conn.GetRules(nfq.table, nfq.chain)
+	assert.NoError(t, err, "conn.GetRules() error = %v", err)
+	assert.Equal(t, 0, len(r), "SendIP4PacketsToDefaultNFQueue() chain should be empty")
 }
 
 func Test_Clean(t *testing.T) {
