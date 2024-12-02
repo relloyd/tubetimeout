b50ff921956d57415604cb6aee85412968c8c037
Richard Lloyd <relloyd@gmail.com>
2024-12-02 21:19:32 +0000

    improve default values

--- config/defaults.go
+++ config/defaults.go
@@ -20,11 +20,11 @@ type AppConfig struct {
 
 type TrackerConfig struct {
 	// Retention is the period for samples to be kept and evaluated.
-	Retention time.Duration `envconfig:"RETENTION" default:"1w"` // 1 week
+	Retention time.Duration `envconfig:"RETENTION" default:"168h"` // 1 week
 	// Granularity is the sampling resolution.
 	Granularity time.Duration `envconfig:"GRANULARITY" default:"1m"`
 	// Threshold is duration for exceeding conditions.
-	Threshold time.Duration `envconfig:"THRESHOLD" default:"3h"`
+	Threshold time.Duration `envconfig:"THRESHOLD" default:"2m"`
 	// StartDay is the day of the week to start the window.
 	StartDay int `envconfig:"START_DAY" default:"5"` // Friday
 	// StartTime is the duration past midnight to start the window.
