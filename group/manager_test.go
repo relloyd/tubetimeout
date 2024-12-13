ced3bd5a4ca5772e825ccb521e4f44c0e242eb0b
Richard Lloyd <relloyd@gmail.com>
2024-12-13 13:13:36 +0000

    fix tests for TestIsSrcIpDestDomainKnown

--- group/manager_test.go
+++ group/manager_test.go
@@ -3,57 +3,38 @@ package group
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
-
 	"example.com/youtube-nfqueue/models"
-	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/assert"
 )
-
-// MockManager to mock IsSrcIpGroupKnown and IsDstDomainGroupKnown
-type MockManager struct {
-	mock.Mock
-	Manager
-}
-
-func (m *MockManager) IsSrcIpGroupKnown(srcIp models.Ip) ([]models.Group, bool) {
-	args := m.Called(srcIp)
-	return args.Get(0).([]models.Group), args.Bool(1)
-}
-
-func (m *MockManager) IsDstDomainGroupKnown(dstDomain models.Domain) ([]models.Group, bool) {
-	args := m.Called(dstDomain)
-	return args.Get(0).([]models.Group), args.Bool(1)
-}
 
 func TestIsSrcIpDestDomainKnown(t *testing.T) {
 	tests := []struct {
-		name                 string
-		srcIp                models.Ip
-		dstDomain            models.Domain
-		managerModeMatchAll  bool
-		mockSrcGroups        []models.Group
-		mockDstGroups        []models.Group
-		mockSrcOk, mockDstOk bool
-		expectedGroups       []models.Group
-		expectedOk           bool
+		name                string
+		srcIp               models.Ip
+		dstDomain           models.Domain
+		managerModeMatchAll bool
+		sourceIpGroups      models.IpGroups
+		destDomainGroups    models.DomainGroups
+		expectedGroups      []models.Group
+		expectedOk          bool
 	}{
 		{
 			name:                "Match all mode - domain known",
 			srcIp:               "192.168.0.1",
 			dstDomain:           "example.com",
 			managerModeMatchAll: true,
-			mockDstGroups:       nil,
-			mockDstOk:           true,
-			expectedGroups:      []models.Group{"192.168.0.1:example.com"},
-			expectedOk:          true,
+			sourceIpGroups:      models.IpGroups{},
+			destDomainGroups: models.DomainGroups{Data: models.MapDomainGroups{"example.com": {"group1"}}},
+			expectedGroups: []models.Group{"192.168.0.1:example.com"},
+			expectedOk:     true,
 		},
 		{
 			name:                "Match all mode - domain unknown",
 			srcIp:               "192.168.0.1",
 			dstDomain:           "unknown.com",
 			managerModeMatchAll: true,
-			mockDstGroups:       nil,
-			mockDstOk:           false,
+			sourceIpGroups:      models.IpGroups{},
+			destDomainGroups:    models.DomainGroups{},
 			expectedGroups:      []models.Group{},
 			expectedOk:          false,
 		},
@@ -62,10 +43,8 @@ func TestIsSrcIpDestDomainKnown(t *testing.T) {
 			srcIp:               "192.168.0.1",
 			dstDomain:           "example.com",
 			managerModeMatchAll: false,
-			mockSrcGroups:       []models.Group{"group1", "group2"},
-			mockDstGroups:       []models.Group{"group2", "group3"},
-			mockSrcOk:           true,
-			mockDstOk:           true,
+			sourceIpGroups:      models.IpGroups{Data: models.MapIpGroups{"192.168.0.1": {"group1", "group2"}}},
+			destDomainGroups:    models.DomainGroups{Data: models.MapDomainGroups{"example.com": {"group2", "group3"}}},
 			expectedGroups:      []models.Group{"group2"},
 			expectedOk:          true,
 		},
@@ -74,22 +53,18 @@ func TestIsSrcIpDestDomainKnown(t *testing.T) {
 			srcIp:               "192.168.0.1",
 			dstDomain:           "example.com",
 			managerModeMatchAll: false,
-			mockSrcGroups:       []models.Group{"group1"},
-			mockDstGroups:       []models.Group{"group2"},
-			mockSrcOk:           true,
-			mockDstOk:           true,
-			expectedGroups:      []models.Group{},
-			expectedOk:          false,
+			sourceIpGroups: models.IpGroups{Data: models.MapIpGroups{"192.168.0.1": {"group1"}}},
+			destDomainGroups: models.DomainGroups{Data: models.MapDomainGroups{"example.com": {"group2"}}},
+			expectedGroups: nil,
+			expectedOk:     false,
 		},
 		{
 			name:                "Either srcIp or dstDomain unknown",
 			srcIp:               "192.168.0.1",
 			dstDomain:           "example.com",
 			managerModeMatchAll: false,
-			mockSrcGroups:       nil,
-			mockDstGroups:       nil,
-			mockSrcOk:           false,
-			mockDstOk:           true,
+			sourceIpGroups:      models.IpGroups{}, // srcIp not known
+			destDomainGroups:    models.DomainGroups{},
 			expectedGroups:      []models.Group{},
 			expectedOk:          false,
 		},
@@ -97,18 +72,21 @@ func TestIsSrcIpDestDomainKnown(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			mockManager := &MockManager{}
-			mockManager.On("IsSrcIpGroupKnown", tt.srcIp).Return(tt.mockSrcGroups, tt.mockSrcOk)
-			mockManager.On("IsDstDomainGroupKnown", tt.dstDomain).Return(tt.mockDstGroups, tt.mockDstOk)
+			// Configure the manager with the test's specific attributes
+			mgr := &Manager{
+				sourceIpGroups:   tt.sourceIpGroups,
+				destDomainGroups: tt.destDomainGroups,
+			}
 
-			managerModeMatchAllSourceIps = tt.managerModeMatchAll // set the global variable
+			// Override the global flag for match-all mode
+			managerModeMatchAllSourceIps = tt.managerModeMatchAll
 
-			actualGroups, actualOk := mockManager.IsSrcIpDestDomainKnown(tt.srcIp, tt.dstDomain)
+			// Run the method under test
+			actualGroups, actualOk := mgr.IsSrcIpDestDomainKnown(tt.srcIp, tt.dstDomain)
 
+			// Assert the results
 			assert.Equal(t, tt.expectedGroups, actualGroups)
 			assert.Equal(t, tt.expectedOk, actualOk)
-
-			mockManager.AssertExpectations(t)
 		})
 	}
 }
