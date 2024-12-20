b973af1e674b996835e1129134e097e9a04413ed
Richard Lloyd <relloyd@gmail.com>
2024-12-20 12:46:55 +0000

    set default threhsold 1m

--- config/defaults.go
+++ config/defaults.go
@@ -27,7 +27,7 @@ type TrackerConfig struct {
 	// Granularity is the sampling resolution.
 	Granularity time.Duration `envconfig:"GRANULARITY" default:"1m"`
 	// Threshold is duration for exceeding conditions.
-	Threshold time.Duration `envconfig:"THRESHOLD" default:"2m"`
+	Threshold time.Duration `envconfig:"THRESHOLD" default:"1m"`
 	// StartDay is the day of the week to start the window.
 	StartDay int `envconfig:"START_DAY" default:"5"` // Friday
 	// StartTime is the duration past midnight to start the window.
