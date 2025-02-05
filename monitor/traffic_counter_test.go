a9ad605e3859a126be8e0c0ddb710586f77938eb
Richard Lloyd <richard.lloyd@paymentsense.com>
2025-02-05 13:30:46 +0000

    fix traffic counter test

--- monitor/traffic_counter_test.go
+++ monitor/traffic_counter_test.go
@@ -35,6 +35,11 @@ func TestTrafficMap(t *testing.T) {
 	assert.Equal(t, tm.trafficMapLen, 0, "unexpected traffic map len initially")
 	assert.Same(t, logger, tm.logger, "unexpected logger")
 
+	// Send some IpMACs.
+	tm.UpdateSourceIpMACs(models.MapIpMACs{
+		testIp: testMac,
+	})
+
 	tm.CountTraffic(testGroup, testIp, models.Ingress, 10, 100)
 	assert.Equal(t, 1, tm.trafficMapLen, "unexpected traffic map len")
 
@@ -51,23 +56,33 @@ func TestTrafficMap(t *testing.T) {
 
 func TestTrafficMap_UpdateSourceIpGroups(t *testing.T) {
 	// TODO: set up MAC data so that keys are removed if they aren't in the new data.
+	testGroup := models.Group("test1")
 	testMac := models.MAC("00:00:00:00:00:00")
+	testMac2 := models.MAC("00:00:00:00:00:01")
+	testIp := models.Ip("1.1.1.1")
+	testIp2 := models.Ip("8.8.8.8")
 
 	logger := config.MustGetLogger()
 	windowSize := 5
+
 	tm := NewTrafficMap(logger, windowSize)
+	tm.UpdateSourceIpMACs(models.MapIpMACs{ // Initial data.
+		testIp:  testMac,
+		testIp2: testMac2,
+	})
 
-	// Known group.
-	testGroup := models.Group("test1")
-	testIp := models.Ip("1.1.1.1")
+	// Known group & IP.
 	tm.CountTraffic(testGroup, testIp, models.Ingress, 10, 100)
+	tm.CountTraffic(testGroup, testIp2, models.Ingress, 10, 100)
 
-	// Unknown group should be removed.
-	tm.CountTraffic("groupA", "8.8.8.8", models.Ingress, 10, 100)
+	// Age out the last active time for the testIp2.
+	data, _ := tm.trafficMap.Load(getTrafficMapKey(testGroup, testMac2))
+	data.(*trafficStats).lastActiveTimeUTC = time.Now().Add(-config.AppCfg.MonitorConfig.PurgeStatsAfterDuration)
 
 	tm.UpdateSourceIpMACs(models.MapIpMACs{
 		testIp: testMac,
+		// omit testIp2 so it is removed.
 	})
-	assert.Equal(t, 1, tm.trafficMapLen, "unexpected traffic map len")
 
+	assert.Equal(t, 1, tm.trafficMapLen, "unexpected traffic map len")
 }
