4c98725b979180ebcd63eee492cc1b50d39747c3
Richard Lloyd <relloyd@gmail.com>
2025-02-07 22:19:39 +0000

    add missing api.go

---
+++ models/api.go
@@ -0,0 +1,29 @@
+package models
+
+import (
+	"time"
+)
+
+// FlatTrackerConfig is used by the API.
+type FlatTrackerConfig struct {
+	Group       Group            `json:"name"`
+	Retention   time.Duration    `json:"retention"` // TODO: get the script to handle retention and threshold
+	Threshold   time.Duration    `json:"threshold"`
+	StartDay    int              `json:"startDay"`
+	Mode        UsageTrackerMode `json:"mode"`
+	ModeEndTime time.Time        `json:"modeEndTime"`
+}
+
+// TrackerMode is used by the API to return data to the web page.
+type TrackerMode struct {
+	Mode        UsageTrackerMode `json:"mode"`
+	ModeEndTime time.Time        `json:"modeEndTime"`
+}
+
+// TrackerSummary contains the used and total count of a group used by the usage tracker and web for reporting.
+type TrackerSummary struct {
+	Used            int               `json:"used"`
+	Total           int               `json:"total"`
+	Percentage      int               `json:"percentage"`
+	LastActiveTimes map[MAC]time.Time `json:"activity"`
+}
